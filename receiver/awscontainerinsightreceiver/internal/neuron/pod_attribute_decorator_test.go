ac59d58012ea328295cb54490889c178cd8910c4
tudorman@amazon.com <tudorman@amazon.com>
2025-07-15 12:04:45 +0100

    Add test for "aws.amazon.com/neuroncore" and "aws.amazon.com/neuron" compatability

--- receiver/awscontainerinsightreceiver/internal/neuron/pod_attribute_decorator_test.go
+++ receiver/awscontainerinsightreceiver/internal/neuron/pod_attribute_decorator_test.go
@@ -225,3 +225,105 @@ func TestConsumeMetricsForPodAttributeDecorator(t *testing.T) {
 
 	decoratorconsumer.RunDecoratorTestScenarios(ctx, t, dc, testcases2)
 }
+
+func TestNeuronDeviceResourceNameCompatibility(t *testing.T) {
+	logger, _ := zap.NewDevelopment()
+	ctx := context.Background()
+
+	testCases := []struct {
+		name         string
+		neuronKey    string
+		storeMock    PodResourcesStoreInterface
+		expectedPod  string
+	}{
+		{
+			name:        "neuroncore_resource_key",
+			neuronKey:   "aws.amazon.com/neuroncore",
+			storeMock:   &mockNeuronCoreStore{},
+			expectedPod: dummyPodName,
+		},
+		{
+			name:        "neuron_resource_key_fallback",
+			neuronKey:   "aws.amazon.com/neuron", 
+			storeMock:   &mockNeuronFallbackStore{},
+			expectedPod: dummyPodNameForAltResource,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			dc := &PodAttributesDecoratorConsumer{
+				NextConsumer:      consumertest.NewNop(),
+				PodResourcesStore: tc.storeMock,
+				Logger:            logger,
+			}
+
+			metrics := decoratorconsumer.GenerateMetrics(map[decoratorconsumer.MetricIdentifier][]map[string]string{
+				{Name: neuronHardwareInfoKey, MetricType: pmetric.MetricTypeSum}: {
+					{neuronCorePerDeviceKey: "2"},
+				},
+				{Name: "test_metric", MetricType: pmetric.MetricTypeGauge}: {
+					{neuronCoreAttributeKey: "1"},
+				},
+			})
+
+			err := dc.ConsumeMetrics(ctx, metrics)
+			if err != nil {
+				t.Errorf("ConsumeMetrics failed for %s: %v", tc.neuronKey, err)
+			}
+
+			// Verify metrics were processed and correct pod attributes were added
+			found := false
+			rms := metrics.ResourceMetrics()
+			for i := 0; i < rms.Len(); i++ {
+				ilms := rms.At(i).ScopeMetrics()
+				for j := 0; j < ilms.Len(); j++ {
+					metricSlice := ilms.At(j).Metrics()
+					for k := 0; k < metricSlice.Len(); k++ {
+						metric := metricSlice.At(k)
+						if metric.Name() == "test_metric" {
+							datapoints := getMetricDatapoints(metric)
+							if datapoints.Len() > 0 {
+								attrs := datapoints.At(0).Attributes()
+								if podName, exists := attrs.Get(ci.K8sPodNameKey); exists {
+									if podName.AsString() == tc.expectedPod {
+										found = true
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			if !found {
+				t.Errorf("Expected pod name %s not found for %s", tc.expectedPod, tc.neuronKey)
+			}
+		})
+	}
+}
+
+type mockNeuronCoreStore struct{}
+
+func (m *mockNeuronCoreStore) GetContainerInfo(index string, resourceName string) *stores.ContainerInfo {
+	if resourceName == neuronCoreResourceName {
+		return &stores.ContainerInfo{
+			PodName:       dummyPodName,
+			ContainerName: dummyContainerName,
+			Namespace:     dummyNamespace,
+		}
+	}
+	return nil
+}
+
+type mockNeuronFallbackStore struct{}
+
+func (m *mockNeuronFallbackStore) GetContainerInfo(index string, resourceName string) *stores.ContainerInfo {
+	if resourceName == neuronDeviceResourceNameAlt {
+		return &stores.ContainerInfo{
+			PodName:       dummyPodNameForAltResource,
+			ContainerName: dummyContainerName,
+			Namespace:     dummyNamespace,
+		}
+	}
+	return nil
+}
